5565626a4934a7d248a68c55c53dc3f048024370
JosueEmilian <juyul@miumg.edu.gt>
2023-03-31 00:55:35 -0600

    ff

--- tienda-frontend/src/axios.js
+++ tienda-frontend/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "store-mern-eight.vercel.app",
 });
 
 export default instance;
+
+//store-mern-gold.vercel.app
